5f2675ff3cd52b09d6a6ef7caf80914b70dae164
Luongdayne <vodailuong2510@gmail.com>
2024-06-10 00:41:20 +0700

    name : taikhoan -> account

--- Web/urls.py
+++ Web/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('taikhoan/', include('taikhoan.urls')),
+    path('account/', include('account.urls')),
     path('', include('coach.urls')),
 ]
 
--- account/apps.py
+++ account/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TaikhoanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'taikhoan'
+    name = 'account'
